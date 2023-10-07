03fea853413fb9c74a6f5b374942c212492b1bb1
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-07 13:00:08 +0900

    Ubah `Routes.php` untuk menambahkan rute baru

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about_index');
+$routes->get('/about/contact', 'Home::about_contact');
+$routes->get('/about/contact/(:any)/(:num)', 'Home::about_contact/$1/$2');
+$routes->get('/about/(:any)/(:num)', 'Home::about_contact/$1/$2');
