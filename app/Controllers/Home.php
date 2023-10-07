04a3a74e8f92c55bf8ae54bba42e2444b0f51f78
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-07 13:00:44 +0900

    Ubah `Controllers/Home.php` untuk menambahkan method baru

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,17 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function about_index(): string
+    {
+        return "Ini contoh dari about";
+    }
+
+    public function about_contact($data = null, $data2 = null): string
+    {
+        if (!isset($data) || !isset($data2)) {
+            return "Ini contoh dari about";
+        }
+        return "Ini contoh dari about yang ada parameter: " . $data . " & " . $data2;
+    }
 }
