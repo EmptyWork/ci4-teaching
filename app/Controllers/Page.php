705f5f91636a1058ee9eb6ec342a514c4f84ea6f
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-12 09:22:53 +0900

    Buat `Page.php` untuk menjadi `Controller`

---
+++ app/Controllers/Page.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Page extends BaseController
+{
+    public function index()
+    {
+        return "ini dari file Page, dengan method index";
+    }
+}
