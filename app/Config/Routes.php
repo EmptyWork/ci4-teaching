3398dc49b636f02a75944cba03c02661c7b733f5
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-14 19:01:10 +0900

    Ubah `Routes.php` urutan rute agar tidak terjadi error

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,5 +18,5 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Page::index');
 $routes->get('/about', 'About::index');
 $routes->get('/about/contact', 'About::contact');
-$routes->get('/about/(:any)/(:num)', 'About::contact/$1/$2');
 $routes->get('/about/contact/(:any)/(:num)', 'About::contact/$1/$2');
+$routes->get('/about/(:any)/(:num)', 'About::contact/$1/$2');
