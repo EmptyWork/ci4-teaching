fef7f9586080ddc0a0b6bd0c0ee1b54887b32961
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-14 18:50:49 +0900

    Ubah `Routes.php` memperbaharui rute berdasarkan controllers yang baru

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/about', 'Home::about_index');
-$routes->get('/about/contact', 'Home::about_contact');
-$routes->get('/about/contact/(:any)/(:num)', 'Home::about_contact/$1/$2');
-$routes->get('/about/(:any)/(:num)', 'Home::about_contact/$1/$2');
+
+// $routes->get('/', 'Home::index');
+// $routes->get('/about', 'Home::about_index');
+// $routes->get('/about/contact', 'Home::about_contact');
+// $routes->get('/about/contact/(:any)/(:num)', 'Home::about_contact/$1/$2');
+// $routes->get('/about/(:any)/(:num)', 'Home::about_contact/$1/$2');
+
+// Kita akan mengubah Routes dari yang diatas menjadi seperti yang ada
+// dibawah ini, dengan membuat 2 Controllers baru, yaitu: Page dan About
+
+$routes->get('/', 'Page::index');
+$routes->get('/about', 'About::index');
+$routes->get('/about/contact', 'About::contact');
+$routes->get('/about/(:any)/(:num)', 'About::contact/$1/$2');
+$routes->get('/about/contact/(:any)/(:num)', 'About::contact/$1/$2');
