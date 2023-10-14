836ac9b8671df79f27f2bd230a492762d0812447
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-14 18:55:22 +0900

    Buat `About\index.php` untuk menjadi tampilan untuk `/about`

---
+++ app/Views/about/index.php
@@ -0,0 +1,8 @@
+<?= $this->extend('layouts/primary') ?>
+
+<?= $this->section("content") ?>
+<main>
+    <h1>Tentang program ini</h1>
+    <p>dibuat hanya untuk menambahkan <code>path</code> pada <strong>controller</strong></p>
+</main>
+<?= $this->endSection('content') ?>
