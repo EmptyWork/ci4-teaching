81724ccc37eb70d6672c65e2a769a1df084f9933
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-18 15:32:11 +0900

    Ubah `Routes.php` untuk menyesuaikan `controllers` akses

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/about', 'Home::about_index');
-$routes->get('/about/contact', 'Home::about_contact');
-$routes->get('/about/contact/(:any)/(:num)', 'Home::about_contact/$1/$2');
-$routes->get('/about/(:any)/(:num)', 'Home::about_contact/$1/$2');
+$routes->get('/', 'Page::index');
+$routes->get('/about', 'About::index');
+$routes->get('/about/contact', 'About::contact');
+$routes->get('/about/contact/(:any)/(:num)', 'About::contact/$1/$2');
+$routes->get('/about/(:any)/(:num)', 'About::contact/$1/$2');
