87b3aae67e49d9f6aec6900cc42bae82a005a667
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-21 08:40:02 +0900

    Ubah `About.php` multi-views untuk tampilan

--- app/Controllers/About.php
+++ app/Controllers/About.php
@@ -8,13 +8,15 @@ class About extends BaseController
 {
     public function index()
     {
-        return "ini dari file About, dengan method index";
+        return view("layouts/header")
+                .view("index")
+                .view("layouts/footer");
     }
 
     public function contact($param1 = null, $param2 = null)
     {
-        if(isset($param1) && isset($param2)) {
-            return "ini dari file About, dengan method contact dan ada parameters, dan data parameternya yaitu: ".$param1." dan ". $param2;
+        if (isset($param1) && isset($param2)) {
+            return "ini dari file About, dengan method contact dan ada parameters, dan data parameternya yaitu: " . $param1 . " dan " . $param2;
         }
 
         return "ini dari file About, dengan method contact";
