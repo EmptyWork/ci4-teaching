aa4a1eec837a88c935ce2af039f7acf11d718f52
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-25 15:37:03 +0900

    Ubah `Page.php` untuk menampilkan data pengguna

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -4,9 +4,22 @@ namespace App\Controllers;
 
 class Page extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('page/index');
+        $pengguna = new \App\Models\Pengguna();
+       
+        $para_pengguna = $pengguna->findAll();
+
+        if(count($para_pengguna) < 1){ 
+            $pengguna->insert([
+                "nama" => "Glend",
+                "alamat" => "Ambon",
+                "email" => "stevarth@emptywork.my.id",
+            ]);
+        }
+
+        return dd($para_pengguna);
+        // return view('page/index');
         // return view('layouts/header') . view('page/index') . view('layouts/footer');
         //return "ini halaman awal dari website yang sedang saya buat dengan menggunakan framework CodeIgniter4 v4.4.1";
     }
