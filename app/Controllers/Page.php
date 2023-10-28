0044e17a76ddfb1641b9619b85ffcac72a4f9b4f
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-10-28 10:55:09 +0900

    Ubah `Page.php` untuk menampilkan data pada view

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -8,9 +8,25 @@ class Page extends BaseController
 {
     public function index()
     {
+        $users = new \App\Models\Users();
+
+        $list_of_users = $users->findAll();
+        
+        if (count($list_of_users) < 1) {
+            $users->insert([
+                "name" => "Glend",
+                "address" => "Ambon"
+            ]);
+            
+            $list_of_users = $users->findAll();
+        }
+        
+        $user_data = $users->where("name", "Glend")->first();
+        return dd($list_of_users, $user_data['name']);
+
         // return view("layouts/header")
         //         .view("multiview/index")
         //         .view("layouts/footer");
-        return view("layouting/index");
+        // return view("layouting/index");
     }
 }
