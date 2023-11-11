86b0ad7e598dd561ece1cb700c0ad05bf5342487
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-11-11 11:46:50 +0900

    Ubah `Routes.php` untuk menambahkan rute `users/*`

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,6 @@ $routes->get('/about', 'About::index');
 $routes->get('/about/contact', 'About::contact');
 $routes->get('/about/contact/(:any)/(:num)', 'About::contact/$1/$2');
 $routes->get('/about/(:any)/(:num)', 'About::contact/$1/$2');
+
+$routes->get('/users', 'UsersController::index');
+$routes->post('/users', 'UsersController::create');
