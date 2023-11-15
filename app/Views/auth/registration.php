dbd1c145cd6502cd614eb0b4d7d9ccd24892ae6f
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-11-15 15:05:54 +0900

    Ubah `auth\registration.php` menambahkan data lama

--- app/Views/auth/registration.php
+++ app/Views/auth/registration.php
@@ -6,9 +6,9 @@
     <h1>Form pembuatan Pengguna baru</h1>
     <form method="post">
         <?= csrf_field() ?>
-        <input type="text" name="nama" id="nama" placeholder="Nama kamu">
-        <input type="email" name="email" id="email" placeholder="Alamat email kamu">
-        <input type="text" name="alamat" id="alamat" placeholder="Alamat rumah kamu">
+        <input type="text" name="nama" id="nama" placeholder="Nama kamu" value="<?= old('nama') ?? "" ?>">
+        <input type="email" name="email" id="email" placeholder="Alamat email kamu" value="<?= old('email') ?? "" ?>">
+        <input type="text" name="alamat" id="alamat" placeholder="Alamat rumah kamu" value="<?= old('alamat') ?? "" ?>">
         <input type="submit" value="Buat Pengguna Baru">
     </form>
 </main>
