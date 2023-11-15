7134193711c7bd1988aeb1970933197262b3f7b7
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-11-15 15:05:02 +0900

    Ubah `Page.php` untuk menampilkan tampilan awal

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -6,20 +6,20 @@ class Page extends BaseController
 {
     public function index()
     {
-        $pengguna = new \App\Models\Pengguna();
+        // $pengguna = new \App\Models\Pengguna();
        
-        $para_pengguna = $pengguna->findAll();
+        // $para_pengguna = $pengguna->findAll();
 
-        if(count($para_pengguna) < 1){ 
-            $pengguna->insert([
-                "nama" => "Glend",
-                "alamat" => "Ambon",
-                "email" => "stevarth@emptywork.my.id",
-            ]);
-        }
+        // if(count($para_pengguna) < 1){ 
+        //     $pengguna->insert([
+        //         "nama" => "Glend",
+        //         "alamat" => "Ambon",
+        //         "email" => "stevarth@emptywork.my.id",
+        //     ]);
+        // }
 
-        return dd($para_pengguna);
-        // return view('page/index');
+        // return dd($para_pengguna);
+        return view('page/index');
         // return view('layouts/header') . view('page/index') . view('layouts/footer');
         //return "ini halaman awal dari website yang sedang saya buat dengan menggunakan framework CodeIgniter4 v4.4.1";
     }
