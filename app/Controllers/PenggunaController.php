a5ba745d9e5cded30d36e1d3232fdfad1719b830
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-11-15 15:06:20 +0900

    Ubah `PenggunaController.php` untuk `users/*/*`

--- app/Controllers/PenggunaController.php
+++ app/Controllers/PenggunaController.php
@@ -60,4 +60,51 @@ class PenggunaController extends BaseController
         session()->setFlashdata('pesan', 'Data berhasil dihapus');
         return redirect()->to('/users/');
     }
+
+    public function edit($id)
+    {
+        $pengguna = new \App\Models\Pengguna();
+        $terpilih = $pengguna->find($id);
+
+        if ($terpilih == null) return redirect()->to("users");
+
+        return view('auth/edit', ['user' => $terpilih]);
+    }
+
+    public function update($id)
+    {
+        if (
+            !$this->validate(
+                [
+                    "nama" => "required",
+                    "email" => "required",
+                    "alamat" => "required"
+                ]
+            )
+        ) {
+            session()->setFlashdata('pesan', 'Data tidak boleh kosong');
+            return redirect()->to('/users/edit/' . $id);
+        }
+
+        $pengguna = new \App\Models\Pengguna();
+        $request = $this->request;
+
+        $pengguna->save([
+            'id' => $id,
+            'nama' => $request->getVar('nama'),
+            'email' => $request->getVar('email'),
+            'alamat' => $request->getVar('alamat'),
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil diubah');
+        return redirect()->to('/users');
+    }
+
+    public function view($id)
+    {
+        $pengguna = new \App\Models\Pengguna();
+        $terpilih = $pengguna->find($id);
+
+        return view("users/view", ["data" => $terpilih]);
+    }
 }
